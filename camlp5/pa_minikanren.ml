85e96e25fa14abd39d7755e9f2b918237a3d3a45
Dmitry Boulytchev <dboulytchev@gmail.com>
2015-10-26 03:10:14 +0300

    Added missing files

--- camlp5/pa_minikanren.ml
+++ camlp5/pa_minikanren.ml
@@ -1,1 +1,55 @@
-let _ = "Oops!"
+(*
+ * pa_minikanren: a camlp5 extension to implement syntax-level
+ * miniKanren constructs.
+ * Copyright (C) 2015
+ * Dmitri Boulytchev, St.Petersburg State University
+ * 
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2, as published by the Free Software Foundation.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU Library General Public License version 2 for more details
+ * (enclosed in the file COPYING).
+ *)
+
+(** {1 Pa_minikanren --- a camlp5 syntax extension for miniKanren syntax constructs} *)
+
+(**
+  {2 General description}
+
+  TODO
+*)
+
+(**/**)
+
+#load "pa_extend.cmo";;
+#load "q_MLast.cmo";;
+
+open Pcaml
+open Printf
+
+EXTEND
+  GLOBAL: expr; 
+
+  expr: LEVEL "top" [
+    [ "fresh"; "("; vars=LIST0 LIDENT; ")"; clauses=LIST1 expr LEVEL "." ->
+      let rec fold f = function 
+      | [h]  -> h
+      | h::t -> f h (fold f t)
+      in
+      let body = fold (fun l r -> <:expr< conj $l$ $r$ >>) clauses in
+      List.fold_right (fun x e -> 
+        let p = <:patt< $lid:x$ >> in 
+        <:expr< call_fresh (fun $p$ -> $e$) >>
+      ) vars body
+    ] |
+    [ "defer"; subj=expr LEVEL "." ->
+      <:expr< (fun __st__ -> MKStream.from_fun (fun () -> $subj$ __st__)) >>
+    ]
+  ];
+
+END;
