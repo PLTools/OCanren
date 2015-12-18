45a7f9dd3b45144ac164df364d9576f03bc16f85
Dmitry Boulytchev <dboulytchev@gmail.com>
2015-12-18 14:14:21 +0300

    More tests

--- regression/test011.ml
+++ regression/test011.ml
@@ -8,20 +8,29 @@ let show_t         = show(logic) (show t)
 let show_int       = show(logic) (show int)
 let show_list      = show(logic) (show list show_int)
 let show_list_list = show(logic) (show list show_list)
+let show_llist     = show(logic) (show llist (show int))
 
 let int_reifier dc x =
   match reify dc x with
   | [] -> ""
   | cs -> show(list) (fun c -> Printf.sprintf "%s =/= %s" (show_int x) (show_int c)) cs
 
-let list_reifier dc = function
-  | (Var _) as x ->
-    begin match reify dc x with
-    | [] -> ""
-    | cs -> show(list) (fun c -> Printf.sprintf "%s =/= %s" (show_list x) (show_list c)) cs
-    end
+let list_reifier dc t = 
+  let h = Hashtbl.create 10 in
+  let check_var i = Hashtbl.mem h i in
+  let add_var   i = Hashtbl.add h i true in
+  match t with
+  | (Var i) as x ->
+      if check_var i 
+      then ""
+      else begin
+	add_var i;
+        match reify dc x with
+	| [] -> ""
+	| cs -> show(list) (fun c -> Printf.sprintf "%s =/= %s" (show_list x) (show_list c)) cs
+      end
   | Value l -> 
-      List.fold_left (fun acc -> function (Var _) as x -> acc ^ int_reifier dc x | _ -> acc) "" l
+      List.fold_left (fun acc -> function (Var i) as x when not (check_var i) -> add_var i; acc ^ int_reifier dc x | _ -> acc) "" l
 
 let _ = 
   run show_t         empty_reifier (-1) q (fun q st -> REPR((fresh(x) (x =/= !(A x)))                                                                   st), ["q", q]);
@@ -42,121 +51,45 @@ let _ =
   run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (x y)(![x; !1] =/= ![!2; y])(x === !2)(y === !1)(![x; y] === q))                    st), ["q", q]);
   run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (a x z)(a =/= ![x; !1])(a === ![z; !1])(x === z))                                   st), ["q", q]);
   run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (a x z)(a =/= ![x; !1])(a === ![z; !1])(x === !5)(![x; z] === q))                   st), ["q", q]);
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((!3 =/= !4)                                                                                st), ["q", q]);
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((!3 =/= !3)                                                                                st), ["q", q]);
+  run show_int       int_reifier   (-1) q (fun q st -> REPR (((!5 =/= q) &&& (!6 =/= q) &&& (q === !5))                                                 st), ["q", q]);
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (a d)(![a; d] === q)(q =/= ![!5; !6])(a === !5))                                    st), ["q", q]);
+  run show_int       int_reifier   (-1) q (fun q st -> REPR ((fresh (a)(!3 === a)(a =/= !4))                                                            st), ["q", q]);
+  run show_int       int_reifier   (-1) q (fun q st -> REPR (((!4 =/= q) &&& (!3 =/= q))                                                                st), ["q", q]);
 
- 
+  (* Extra constraint *)
+  run show_int       int_reifier   (-1) q (fun q st -> REPR (((q =/= !5) &&& (q =/= !5))                                                                st), ["q", q]);
+  run show_int       int_reifier   (-1) q (fun q st -> REPR ((let foo x = fresh (a)(x =/= a) in fresh(a)(foo a))                                        st), ["q", q]);
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (x y)(![x; y] === q)(x =/= y))                                                      st), ["q", q]);
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (a d)(![a; d] === q)(q =/= ![!5; !6]))                                              st), ["q", q]);
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (a d)(![a; d] === q)(q =/= ![!5; !6])(a === !3))                                    st), ["q", q]);
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (x y)(![x; y] === q)(y =/= x))                                                      st), ["q", q]);
+
+  (* Extra constraint *)
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (x y)(![x; y] === q)(x =/= y)(y =/= x))                                             st), ["q", q]);
+
+  (* Extra constraint *)
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (x y)(![x; y] === q)(x =/= y)(x =/= y))                                             st), ["q", q]);
+
+  (* Extra constraint *)
+  run show_int       int_reifier   (-1) q (fun q st -> REPR (((q =/= !5) &&& (!5 =/= q))                                                                st), ["q", q]);
+
 (*
-
-(test "=/=-28"
-  (run* (q)
-    (=/= 3 4))
-  '(_.0))
-
-(test "=/=-29"
-  (run* (q)
-    (=/= 3 3))
-  '())
-
-(test "=/=-30"
-  (run* (q) (=/= 5 q)
-	    (=/= 6 q)
-	    (== q 5))
-  '())
-
-(test "=/=-31"
-  (run* (q)
-  (fresh (a d)
-    (== `(,a . ,d) q)
-    (=/= q `(5 . 6))
-    (== a 5)))
-  '(((5 . _.0) (=/= ((_.0 6))))))
-
-(test "=/=-32"
-  (run* (q)
-    (fresh (a)
-      (== 3 a)
-      (=/= a 4)))
-  '(_.0))
-
-(test "=/=-33"
-  (run* (q)
-    (=/= 4 q)
-    (=/= 3 q))
-  '((_.0 (=/= ((_.0 3)) ((_.0 4))))))
-
-(test "=/=-34"
-  (run* (q) (=/= q 5) (=/= q 5))
-  '((_.0 (=/= ((_.0 5))))))
-
-(test "=/=-35"
-  (let ((foo (lambda (x)
-               (fresh (a)
-                 (=/= x a)))))
-    (run* (q) (fresh (a) (foo a))))
-  '(_.0))
-
-(test "=/=-36"
-  (let ((foo (lambda (x)
-               (fresh (a)
-                 (=/= x a)))))
-    (run* (q) (fresh (b) (foo b))))
-  '(_.0))
-
-(test "=/=-37"
-  (run* (q)
-    (fresh (x y)
-      (== `(,x ,y) q)
-      (=/= x y)))
-  '(((_.0 _.1) (=/= ((_.0 _.1))))))
-
-(test "=/=-37b"
-  (run* (q)
-  (fresh (a d)
-    (== `(,a . ,d) q)
-    (=/= q `(5 . 6))))
-  '(((_.0 . _.1) (=/= ((_.0 5) (_.1 6))))))
-
-(test "=/=-37c"
-  (run* (q)
-  (fresh (a d)
-    (== `(,a . ,d) q)
-    (=/= q `(5 . 6))
-    (== a 3)))
-  '((3 . _.0)))
-
-(test "=/=-38"
-  (run* (q)
-    (fresh (x y)
-      (== `(,x ,y) q)
-      (=/= y x)))
-  '(((_.0 _.1) (=/= ((_.0 _.1))))))
-
-(test "=/=-39"
-  (run* (q)
-    (fresh (x y)
-      (== `(,x ,y) q)
-      (=/= x y)
-      (=/= y x)))
-  '(((_.0 _.1) (=/= ((_.0 _.1))))))
-
-(test "=/=-40"
-  (run* (q)
-    (fresh (x y)
-      (== `(,x ,y) q)
-      (=/= x y)
-      (=/= x y)))
-  '(((_.0 _.1) (=/= ((_.0 _.1))))))
-
-(test "=/=-41"
-  (run* (q) (=/= q 5) (=/= 5 q))
-  '((_.0 (=/= ((_.0 5))))))
-
 (test "=/=-42"
   (run* (q)
     (fresh (x y)
       (== `(,x ,y) q)
       (=/= `(,x ,y) `(5 6))
       (=/= x 5)))
-  '(((_.0 _.1) (=/= ((_.0 5))))))
+  '(((_.0 _.1) (=/= ((_.0 5)))))) <- why not (=/= ((_.0 5)(_.1 6))) ?
+*)
+  (* Extra constraint *)
+  run show_list      list_reifier  (-1) q (fun q st -> REPR ((fresh (x y)(![x; y] === q)(![x; y] =/= ![!5; !6])(x =/= !5))                              st), ["q", q]);
+ 
+
+(*
+This test also suspicios:
 
 (test "=/=-43"
   (run* (q)
@@ -189,83 +122,57 @@ let _ =
       (=/= `( ,y ,x) `(6 5))
       (== `(,x ,y) q)))
   '(((_.0 _.1) (=/= ((_.0 5))))))
-
-(test "=/=-47"
-  (run* (x)
-    (fresh (y z)
-      (=/= x `(,y 2))
-      (== x `(,z 2))))
-  '((_.0 2)))
-
-(test "=/=-48"
-  (run* (x)
-    (fresh (y z)
-      (=/= x `(,y 2))
-      (== x `((,z) 2))))
-  '(((_.0) 2)))
-
-(test "=/=-49"
-  (run* (x)
-    (fresh (y z)
-      (=/= x `((,y) 2))
-      (== x `(,z 2))))
-  '((_.0 2)))
-
-(define distincto
-  (lambda (l)
-    (conde
-      ((== l '()))
-      ((fresh (a) (== l `(,a))))
-      ((fresh (a ad dd)
-         (== l `(,a ,ad . ,dd))
-         (=/= a ad)
-         (distincto `(,a . ,dd))
-         (distincto `(,ad . ,dd)))))))
-
-(test "=/=-50"
-   (run* (q)
-     (distincto `(2 3 ,q)))
-   '((_.0 (=/= ((_.0 2)) ((_.0 3))))))
-
-(define rembero
-  (lambda (x ls out)
-    (conde
-      ((== '() ls) (== '() out))
-      ((fresh (a d res)
-         (== `(,a . ,d) ls)
-         (rembero x d res)
-         (conde
-           ((== a x) (== out res))
-           ((== `(,a . ,res) out))))))))
-
-(test "=/=-51"
-  (run* (q) (rembero 'a '(a b a c) q))
-  '((b c) (b a c) (a b c) (a b a c)))
-
-(test "=/=-52"
-  (run* (q) (rembero 'a '(a b c) '(a b c)))
-  '(_.0))
-
-(define rembero
-  (lambda (x ls out)
-    (conde
-      ((== '() ls) (== '() out))
-      ((fresh (a d res)
-         (== `(,a . ,d) ls)
-         (rembero x d res)
-         (conde
-           ((== a x) (== out res))
-           ((=/= a x) (== `(,a . ,res) out))))))))
-
-(test "=/=-53"
-  (run* (q) (rembero 'a '(a b a c) q))
-  '((b c)))
-
-(test "=/=-54"
-  (run* (q) (rembero 'a '(a b c) '(a b c)))
-  '())
-
-(test "=/=-55"
-  (run 1 (q) (=/= q #f))
-  '((_.0 (=/= ((_.0 #f))))))
 *)
+
+  (* Extra constraint *)
+  run show_list      list_reifier  (-1) q (fun x st -> REPR ((fresh (y z)(x =/= ![y; !2])(x === ![z; !2]))                                                      st), ["x", x]);
+
+  let rec distincto l =
+    conde [
+      l === !Nil;
+      (fresh (a) (l === !< a));
+      (fresh (a ad dd) (
+         (l === a % (ad % dd)) &&&
+         (a =/= ad) &&&
+         (distincto (a % dd)) &&&
+         (distincto (ad % dd))
+      ))
+    ]
+
+   in
+
+   run show_int      int_reifier  (-1) q (fun q st -> REPR (distincto (!2 % (!3 %< q))                                        st), ["q", q]);
+
+   let rec rembero x ls out =
+     conde [
+       (ls === !Nil) &&& (out === !Nil);
+       fresh (a d res) (
+         (ls === a % d) &&&
+         (rembero x d res) &&&
+         (conde [
+             (a === x) &&& (out === res);
+             (out === a % res)   
+          ])
+       )
+     ]
+   in 
+
+   run show_llist    empty_reifier  (-1) q (fun q st -> REPR (rembero !1 (!1 % (!2 % (!1 %< !3))) q                                        st), ["q", q]);
+   run show_llist    empty_reifier  (-1) q (fun q st -> REPR (rembero !1 (!1 % (!2 %< !3)) (!1 % (!2 %< !3))                                       st), ["q", q]);
+
+   let rec rembero x ls out =
+     conde [
+       (ls === !Nil) &&& (out === !Nil);
+       fresh (a d res) (
+         (ls === a % d) &&&
+         (rembero x d res) &&&
+         (conde [
+             (a === x) &&& (out === res);
+             (a =/= x) &&& (out === a % res)   
+          ])
+       )
+     ]
+   in 
+
+   run show_llist    empty_reifier  (-1) q (fun q st -> REPR (rembero !1 (!1 % (!2 % (!1 %< !3))) q                                        st), ["q", q]);
+   run show_llist    empty_reifier  (-1) q (fun q st -> REPR (rembero !1 (!1 % (!2 %< !3)) (!1 % (!2 %< !3))                                       st), ["q", q])
