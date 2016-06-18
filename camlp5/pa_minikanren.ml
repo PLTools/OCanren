79d343525758e9f5b917cfae3a10b2103a99b19a
Dmitry Boulytchev <dboulytchev@gmail.com>
2016-06-19 02:05:33 +0300

    Better README.md

--- camlp5/pa_minikanren.ml
+++ camlp5/pa_minikanren.ml
@@ -21,7 +21,12 @@
 (**
   {2 General description}
 
-  TODO
+  There are two syntat extensions provided: [fresh] and [defer].
+
+  [fresh] is a direct analog to the corresponding construct in original miniKanren. It has the form
+  [fresh (x y ...) g], where [x y ...] is a list of free variables, created by [fresh], [g] --- some goal.
+
+  [defer] performs "inverse-eta-delay". It has the form [defer (g)] and expanded into [fun st -> Lazy.from_fun (fun () -> g st)].
 *)
 
 (**/**)
