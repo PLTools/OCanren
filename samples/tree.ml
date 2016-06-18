0fa14336213c6b08294fee8c5fcb576ac2828336
Dmitry Boulytchev <dboulytchev@gmail.com>
2016-06-19 02:00:25 +0300

    Better README.md

--- samples/tree.ml
+++ samples/tree.ml
@@ -1,19 +1,27 @@
 open GT
 open MiniKanren
 
+(* Abstracted type for the tree *)
 @type ('a, 'self) tree = Nil | Node of 'a * 'self * 'self with gmap, show
 
+(* A shortcut for "ground" tree we're going to work with in "functional" code *)
 type gtree = (int, gtree) tree
+
+(* Logic counterpart *)
 type ltree = (Nat.logic, ltree) tree logic
 
+(* Printing ground tree function *)
 let rec show_tree t = show(tree) (show(int)) show_tree t
 
+(* Injection *)
 let rec inj_tree : gtree -> ltree = fun t ->
   !! (gmap(tree) inj_nat inj_tree t)
 
+(* Projection *)
 let rec prj_tree : ltree -> gtree = fun t ->
   gmap(tree) prj_nat prj_tree (prj t)
 
+(* Relational insert into a search tree *)
 let rec inserto a t t' = conde [
   (t === !!Nil) &&& (t' === !!(Node (a, !!Nil, !!Nil)));
   fresh (x l r l')
@@ -25,14 +33,18 @@ let rec inserto a t t' = conde [
     ])
 ]
 
+(* Top-level wrapper for insertion --- takes and returns non-logic data *)
 let insert a t =
   run q (fun q  -> inserto (inj_nat a) (inj_tree t) q)
         (fun qs -> prj_tree @@ Stream.hd qs)
 
+(* Top-level wrapper for "inverse" insertion --- returns an integer, which
+   has to be inserted to convert t into t' *)
 let insert' t t' =
   run q (fun q  -> inserto q (inj_tree t) (inj_tree t'))
         (fun qs -> prj_nat @@ Stream.hd qs)  
 
+(* Entry point *)
 let _ =
   let insert_list l =
     let rec inner t = function 
