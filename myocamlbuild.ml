4afe6022b05d6825fa44bcf1be1055f7b965d3de
kakadu <kakadu.hafanana@gmail.com>
2017-02-17 17:51:33 +0300

    Added flag to keep generated assembly

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -174,6 +174,8 @@ let () = dispatch (function
      (*      ] *)
      (*   ); *)
 
+     flag ["ocaml";"compile";"native";"keep_asm"] (S[A"-S"]);
+
      (* cppo-related stuff *)
      let cppo_rules ext =
        let dep   = "%(name).cppo"-.-ext
