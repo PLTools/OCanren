84da17bb41dae8a8e0ba558210b807f300e71e5c
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2017-03-15 22:36:40 +0300

    Rename some stuff as in article:
    reification_rez               ---> refined
    reification_rez#has_free      ---> refined#is_open
    reification_rez#as_plain_exn  ---> refined#prj
    reification_rez#as_logic      ---> refined#refine
    HasFreeVariables              ---> Not_a_value (old-style)

--- regression/test000.ml
+++ regression/test000.ml
@@ -4,41 +4,41 @@ open Printf
 open ManualReifiers
 open GT
 
-module Option = 
+module Option =
   struct
 
-    module T = 
-      struct    
+    module T =
+      struct
         type 'a t = 'a option
-        let fmap f x = gmap(option) f x 
+        let fmap f x = gmap(option) f x
       end
 
-  include T 
-  include Fmap1(T)
+    include T
+    include Fmap1(T)
 
-  let some x  = inj @@ distrib (Some x)
-  let none () = inj @@ distrib None 
-end
+    let some x  = inj @@ distrib (Some x)
+    let none () = inj @@ distrib None
+  end
 
 let show_int       = show(int)
 let show_int_opt   = show(option) (show(int))
 let show_intl      = show(logic)  (show(int))
-let show_intl_optl = show(logic)  (show(option) (show(logic) (show(int)))) 
+let show_intl_optl = show(logic)  (show(option) (show(logic) (show(int))))
 
 let int_opt_reifier = Option.reify int_reifier
 
-let _ =
-  let open Option in
-  run_exn show_int 1 q qh (REPR(fun q -> q === !!5));
-  runR int_opt_reifier show_int_opt show_intl_optl 1 q qh (REPR(fun q -> q === some !!5));
-  runR int_opt_reifier show_int_opt show_intl_optl 1 q qh (REPR(fun q -> q === none ()));
-  runR int_reifier     show_int     show_intl      1 q qh (REPR(fun q -> some q === some !!5 ));
-  runR int_opt_reifier show_int_opt show_intl_optl 1 q qh (REPR(fun q -> call_fresh (fun w -> q === some w)))  
+let _ = Option.(
+    run_exn show_int 1 q qh (REPR(fun q -> q === !!5));
+    runR int_opt_reifier show_int_opt show_intl_optl 1 q qh (REPR(fun q -> q === some !!5));
+    runR int_opt_reifier show_int_opt show_intl_optl 1 q qh (REPR(fun q -> q === none ()));
+    runR int_reifier     show_int     show_intl      1 q qh (REPR(fun q -> some q === some !!5 ));
+    runR int_opt_reifier show_int_opt show_intl_optl 1 q qh (REPR(fun q -> call_fresh (fun w -> q === some w)))
+  )
 
-module Result = 
+module Result =
   struct
 
-    module X = 
+    module X =
       struct
         @type ('a,'b) t = Ok of 'a | Error of 'b with show, gmap
         let fmap f g x = gmap(t) f g x
@@ -63,6 +63,5 @@ let _ =
   runResult   (-1) q qh (REPR(fun q -> Fresh.two (fun r s -> conde
                                                                 [ (q === Result.ok    s) &&& (s =/= !!4)
                                                                 ; (q === Result.error r)
-                                                                ]) 
+                                                                ])
                         ))
-
