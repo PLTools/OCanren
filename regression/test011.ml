74053682c5ba3f9b7213db8b09985fbaaa8a9940
kakadu <kakadu.hafanana@gmail.com>
2017-04-10 21:47:17 +0300

    Refactor constraints interaction

--- regression/test011.ml
+++ regression/test011.ml
@@ -5,11 +5,14 @@ open Tester
 let (!) = inj_int
 let (!!) = List.of_list
 
-let show_int_list   = GT.(show List.ground @@ show int)
-let show_intl_llist = GT.(show List.logic @@ show logic @@ show int)
+let show_int_list         = GT.(show List.ground @@ show int)
+let show_int_list_list    = GT.(show List.ground @@ show List.ground @@ show int)
+let show_intl_llist       = GT.(show List.logic @@ show logic @@ show int)
+let show_intl_llist_llist = GT.(show List.logic @@ show List.logic @@ show logic @@ show int)
 
 let runInt n = runR ManualReifiers.int_reifier GT.(show int) GT.(show logic @@ show int) n
 let runIList n = runR (List.reify ManualReifiers.int_reifier) show_int_list show_intl_llist n
+let runIListList n = runR (List.reify @@ List.reify ManualReifiers.int_reifier) show_int_list_list show_intl_llist_llist n
 
 let _ =
   runInt       (-1) q qh (REPR (fun q -> (q =/= !1)       ));
@@ -25,9 +28,7 @@ let _ =
   ()
 let _ =
   runIList      (-1) q qh (REPR (fun q -> (fresh (x y)(!![x; !1] =/= !![!2; y])(x === !2))                                       ));
-  runIList      (-1) q qh (REPR (fun q -> (fresh (x y)(!![x; !1] =/= !![!2; y])(y === !1))                                       ))
-
-let _ =
+  runIList      (-1) q qh (REPR (fun q -> (fresh (x y)(!![x; !1] =/= !![!2; y])(y === !1))                                       ));
   runIList      (-1) q qh (REPR (fun q -> (fresh (x y)
                                                     (!![x; !1] =/= !![!2; y])
                                                     (!![x; y] === q))
@@ -37,10 +38,12 @@ let _ =
   runIList      (-1) q qh (REPR (fun q -> (fresh (x y)(!![x; !1] =/= !![!2; y])(x === !2)(y === !9)(!![x; y] === q))           ));
   runIList      (-1) q qh (REPR (fun q -> (fresh (a d)(!![a; d] === q)(q =/= !![!5; !6])(a === !5)(d === !6))                  ));
   runIList      (-1) q qh (REPR (fun q -> (fresh (x y)(!![x; !1] =/= !![!2; y])(x === !2)(y === !1)(!![x; y] === q))           ));
-  runIList      (-1) q qh (REPR (fun q -> (fresh (a x z)(a =/= !![x; !1])(a === !![z; !1])(x === z))                           ))
+  runIList      (-1) q qh (REPR (fun q -> (fresh (a x z)(a =/= !![x; !1])(a === !![z; !1])(x === z))                           ));
+  ()
 
 let _ =
-  runIList      (-1) q qh (REPR (fun q -> (fresh (a x z)(a =/= !![x; !1])(a === !![z; !1])(x === !5)(!![x; z] === q))         ))
+  runIList      (-1) q qh (REPR (fun q -> (fresh (a x z)(a =/= !![x; !1])(a === !![z; !1])(x === !5)(!![x; z] === q))         ));
+  ()
 
 let _ =
   runInt                (-1) q qh (REPR (fun q -> (!3 =/= !4) ));
@@ -52,7 +55,7 @@ let _ =
   runInt                (-1) q qh (REPR (fun q -> ((q =/= !5) &&& (q =/= !5))                              ));
 
   runInt                (-1) q qh (REPR (fun q -> (let foo x = fresh (a)(x =/= a) in fresh(a)(foo a))      ));
-  runIList            (-1) q qh (REPR (fun q -> (fresh (x y)(!![x; y] === q)(x =/= y))                                        ));
+  runIList            (-1) q qh (REPR (fun q -> (fresh (x y)(!![x; y] === q)(x =/= y))                     ));
   runIList            (-1) q qh (REPR (fun q -> Fresh.two (fun a d -> ?& [ !![a; d] === q; q =/= !![!5; !6] ]) ));
   runIList            (-1) q qh (REPR (fun q -> Fresh.two (fun a d -> ?& [ !![a; d] === q; q =/= !![!5; !6];
                                                                           (a === !3) ])                    ));
@@ -68,8 +71,20 @@ let _ =
   runIList           (-1) q qh (REPR (fun q -> (fresh (x y)(!5 =/= x)(!![y; x] =/= !![!6; !5])(!![x; y] === q))                 ));
 
   let xsh xs = ["xs", xs] in
-  runIList (-1) q xsh (REPR (fun x -> (fresh (y z)(x =/= !![y; !2])(x === !![z; !2]))))
+  runIList (-1) q xsh (REPR (fun x -> (fresh (y z)(x =/= !![y; !2])(x === !![z; !2]))));
 
+  runIListList (-1) q qh (REPR (fun q -> (fresh (x y z)(q =/= x)(x =/= !![y])(y =/= !![z]) )           ));
+  ()
+
+
+(* next test goes into stackoverflow in ICFP 2016 version *)
+let () =
+  let runL n = runR (List.reify @@ List.reify ManualReifiers.int_reifier) (fun _ -> assert false)
+                (GT.show List.logic (GT.show List.logic @@ GT.show logic string_of_int ))
+                n in
+  let wrap x y = Fresh.one (fun tempH -> x =/= tempH % y) in
+  runL              (-1) q qh (REPR (fun q -> (fresh (x y z)(x === q)(wrap x y)(wrap y z) (wrap z x) ) ));
+  ()
 
 let () =
   let rec distincto l =
