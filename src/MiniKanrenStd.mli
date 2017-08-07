44610d4a60259c759b2763dca747bed1559cecfb
Dmitry Boulytchev <dboulytchev@gmail.com>
2017-08-08 01:34:37 +0300

    Implemented normal Option and Pair

--- src/MiniKanrenStd.mli
+++ src/MiniKanrenStd.mli
@@ -49,6 +49,65 @@ val triple : ('a, 'd) injected -> ('b, 'e) injected -> ('c,'f) injected -> ('a *
 | O
 | S of 'a with show, html, eq, compare, foldl, foldr, gmap
 
+(** {3 Relational pairs} *)
+module Pair' : 
+  sig
+
+    (** Type synonym to prevent toplevel [logic] from being hidden *)
+    type 'a logic' = 'a logic
+
+    (** Synonym for regular option type *)
+    type ('a, 'b) t = 'a * 'b
+
+    (** Ground option (the regular one) *)
+    type ('a, 'b) ground = 'a * 'b
+
+    (** Logic option *)
+    type ('a, 'b) logic = ('a * 'b) logic'
+
+    (** GT-compatible typeinfo for [ground] *)
+    val ground :
+      (unit,
+       < compare : ('a -> 'a -> GT.comparison) -> ('b -> 'b -> GT.comparison) -> ('a, 'b) ground -> ('a, 'b) ground -> GT.comparison;
+         eq      : ('a -> 'a -> bool) -> ('b -> 'b -> bool) -> ('a, 'b) ground -> ('a, 'b) ground -> bool;
+         foldl   : ('c -> 'a -> 'c) -> ('c -> 'b -> 'c) -> 'c -> ('a, 'b) ground -> 'c;
+         foldr   : ('c -> 'a -> 'c) -> ('c -> 'b -> 'c) -> 'c -> ('a, 'b) ground -> 'c;
+         gmap    : ('a -> 'c) -> ('b -> 'd) -> ('a, 'b) ground -> ('c, 'd) ground;
+         html    : ('a -> HTML.viewer) -> ('b -> HTML.viewer) -> ('a, 'b) ground -> HTML.viewer;
+         show    : ('a -> string) -> ('b -> string) -> ('a, 'b) ground -> string >)
+      GT.t
+
+    (** GT-compatible typeinfo for [logic] *)
+    val logic :
+      (unit,
+       < compare : ('a -> 'a -> GT.comparison) -> ('b -> 'b -> GT.comparison) -> ('a, 'b) logic -> ('a, 'b) logic -> GT.comparison;
+         eq      : ('a -> 'a -> bool) -> ('b -> 'b -> bool) -> ('a, 'b) logic -> ('a, 'b) logic -> bool;
+         foldl   : ('c -> 'a -> 'c) -> ('c -> 'b -> 'c) -> 'c -> ('a, 'b) logic -> 'c;
+         foldr   : ('c -> 'a -> 'c) -> ('c -> 'b -> 'c) -> 'c -> ('a, 'b) logic -> 'c;
+         gmap    : ('a -> 'c) -> ('b -> 'd) -> ('a, 'b) logic -> ('c, 'd) logic;
+         html    : ('a -> HTML.viewer) -> ('b -> HTML.viewer) -> ('a, 'b) logic -> HTML.viewer;
+         show    : ('a -> string) -> ('b -> string) -> ('a, 'b) logic -> string >)
+      GT.t
+
+    (** Logic injection (for reification) *)
+    val inj : ('a -> 'c) -> ('b -> 'd) -> ('a, 'b) ground -> ('c, 'd) logic
+
+    (** A synonym for injected pair *)
+    type ('a, 'b, 'c, 'd) groundi = (('a, 'b) ground, ('c, 'd) logic) injected
+
+    (** Make injected pair from ground one with injected components *)
+    val pair : ('a, 'b) injected -> ('c, 'd) injected -> ('a, 'b, 'c, 'd) groundi
+
+(*
+    (** Reifier *)
+    val reify : (helper -> ('a, 'b) injected -> 'b) -> helper -> ('a, 'b) groundi -> 'b logic
+
+    (** Injection counterpart for constructors *)
+    val some : ('a, 'b) injected -> ('a, 'b) groundi
+    val none : unit -> ('a, 'b) groundi
+*)
+  end
+
 (** {3 Relational [option]} *)
 module Option : 
   sig
