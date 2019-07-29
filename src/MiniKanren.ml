cc93028f76a0a74ecb5763eebff5387dfe688b85
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2019-07-29 14:54:17 +0300

    fix

--- src/MiniKanren.ml
+++ src/MiniKanren.ml
@@ -22,11 +22,41 @@ include Core
 
 module Std =
   struct
-    include Std
 
     module LPair    = LPair
     module LOption  = LOption
     module LBool    = LBool
     module LNat     = LNat
     module LList    = LList
+
+    let eqo x y t =
+      conde [
+        (x === y) &&& (t === LBool.truo);
+        (x =/= y) &&& (t === LBool.falso);
+      ]
+
+    let neqo x y t =
+      conde [
+        (x =/= y) &&& (t === LBool.truo);
+        (x === y) &&& (t === LBool.falso);
+      ]
+
+    let nat n = LNat.nat (LNat.of_int n)
+
+    let (%)  = LList.cons
+    let (%<) = LList.(%<)
+    let (!<) = LList.(!<)
+    let nil  = LList.nil
+
+    let rec list f = function
+    | []    -> nil ()
+    | x::xs -> LList.cons (f x) (list f xs)
+
+    let rec nat_list = function
+    | []    -> nil ()
+    | x::xs -> nat x % nat_list xs
+
+    let some = LOption.some
+    let none = LOption.none
+    let pair = LPair.pair
   end
