ac64be1115f1592342de46a4cd0f2efa3c2d99fd
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2019-12-20 13:50:43 +0300

    WIP: test.sh script

--- test.sh
+++ test.sh
@@ -3,53 +3,93 @@
 # simple script for running test and diffing its result with the expected;
 # please, run the script from the toplevel directory of the project
 
-set -e
+# predefined directory with tests
+TESTDIR="regression"
+
+# path to the file containing list of tests
+TESTLIST="${TESTDIR}/tests.txt"
+
+# directories to search for tests
+SEARCH_DIRS="${TESTDIR} samples"
+
+PROMOTE=0
+ARG=""
+
+USAGE_MSG="usage: $0 [--promote] (<test name> | all)"
 
 if test $# = 0; then
-    echo "usage: $0 <test name>"
+    echo "${USAGE_MSG}"
     exit 1
 fi
 
-TEST=$1
-
-TESTDIRS="regression samples"
-
-FOUND=0
-
-for DIR in ${TESTDIRS}; do
-  TESTDIR=${DIR}
-  TESTSRC="${TESTDIR}/${TEST}.ml"
-  if [[ -e ${TESTSRC} ]]; then
-    FOUND=1
-    break
+if [[ $1 == "--promote" ]]; then
+  if test $# = 1; then
+    echo "${USAGE_MSG}"
+    exit 1
   fi
-done
-
-if [[ ${FOUND} -eq "0" ]]; then
-  echo "cannot find ${TEST}"
-  echo "searched in ${TESTDIRS}"
-  exit
-fi
-
-TESTEXE="${TESTDIR}/${TEST}.exe"
-TESTLOG="${TESTDIR}/${TEST}.log"
-TESTDIFF="${TESTDIR}/${TEST}.diff"
-TESTORIG="${TESTDIR}/orig/${TEST}.orig"
-
-# run the test;
-# we use `dune exec` so that `dune`
-# can build the test and its dependencies
-# if it has not been done
-
-dune exec --no-print-directory ${TESTEXE} > ${TESTLOG}
-
-# diff test output with the expected and print `PASSED/FAILED` message
-
-if diff -u ${TESTORIG} ${TESTLOG} > ${TESTDIFF}; then
-    echo "${TEST}: PASSED"
-    rm -f ${TESTDIFF}
-    exit 0
+  PROMOTE=1
+  ARG=$2
 else
-    echo "${TEST}: FAILED (see ${TESTDIFF})"
-    exit 1
+  ARG=$1
 fi
+
+if [[ ${ARG} == "all" ]]; then
+  # read test names from the file
+  TESTS=`cat ${TESTLIST}`
+else
+  # try to find test/sample in predefined directories
+  FOUND=0
+
+  for DIR in ${SEARCH_DIRS}; do
+    TESTDIR=${DIR}
+    TESTSRC="${TESTDIR}/${ARG}.ml"
+    if [[ -f ${TESTSRC} ]]; then
+      FOUND=1
+      break
+    fi
+  done
+
+  if [[ ${FOUND} -eq "0" ]]; then
+    echo "cannot find ${ARG}"
+    echo "searched in ${SEARCH_DIRS}"
+    exit
+  fi
+
+  TESTS=${ARG}
+
+fi
+
+for TEST in ${TESTS}; do
+
+  TESTEXE="${TESTDIR}/${TEST}.exe"
+  TESTLOG="${TESTDIR}/${TEST}.log"
+  TESTDIFF="${TESTDIR}/${TEST}.diff"
+  TESTORIG="${TESTDIR}/orig/${TEST}.orig"
+
+  # if the `--promote` option set, perform the promotion
+  if [[ ${PROMOTE} -eq "1" ]]; then
+    if [[ ! -f ${TESTLOG} ]]; then
+      echo "cannot find ${TESTLOG}"
+      exit 1
+    fi
+    cp ${TESTLOG} ${TESTORIG}
+    continue
+  fi
+
+  # run the test;
+  # we use `dune exec` so that `dune`
+  # can build the test and its dependencies
+  # if it has not been done yet
+
+  dune exec --no-print-directory ${TESTEXE} > ${TESTLOG}
+
+  # diff test output with the expected and print `PASSED/FAILED` message
+
+  if diff -u ${TESTORIG} ${TESTLOG} > ${TESTDIFF}; then
+      echo "${TEST}: PASSED"
+      rm -f ${TESTDIFF}
+  else
+      echo "${TEST}: FAILED (see ${TESTDIFF})"
+  fi
+
+done
