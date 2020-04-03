Kakadu/miniKanren-ocaml-typed

Added structural constraint

src/OCanren.ml
@@ -62,4 +62,8 @@ module Std =
     let some = Option.some
     let none = Option.none
     let pair = Pair.pair
+
+
+    let fortytwo = Core.structural
+
   end

PLTools/OCanren

src/OCanren.ml
@@ -60,4 +60,8 @@ module Std =
     let some = Option.some
     let none = Option.none
     let pair = Pair.pair
+
+
+    let structural = Core.structural
+
   end