80bcfb155d74dd413644a81172e958251519563c
Kakadu <Kakadu@pm.me>
2021-08-13 02:05:11 +0300

    Minimizing example 22
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- regression/test022newsyntax.ml
+++ regression/test022newsyntax.ml
@@ -1,12 +1,10 @@
 open OCanren
 open OCanren.Std
 
-let f n = Nat.(<=) 0 n
+(* let f n = Nat.(<=) 0 n *)
 
 let _ =
   Stdlib.List.iter (fun n -> print_endline (GT.show Std.Nat.ground n)) @@
     Stream.take ~n:1 @@
       run q (fun n ->
-          ocanren {fresh c     in Nat.(<=) 0 n
-                                & Nat.(<=) n 0
-                                & n == Nat.zero }) project
+          ocanren {  Nat.(<=) 0 n }) project
