Kakadu/miniKanren-ocaml-typed

WIP on wildcards
Signed-off-by: Kakadu <Kakadu@pm.me>

src/core/Core.ml
@@ -33,13 +33,13 @@ let stat = {
     disj_counter      = 0;
     delay_counter     = 0
 }
-         
+
 let unification_counter () = stat.unification_count
-let unification_time    () = stat.unification_time 
-let conj_counter        () = stat.conj_counter 
-let disj_counter        () = stat.disj_counter 
-let delay_counter       () = stat.delay_counter 
-                          
+let unification_time    () = stat.unification_time
+let conj_counter        () = stat.conj_counter
+let disj_counter        () = stat.disj_counter
+let delay_counter       () = stat.delay_counter
+
 let unification_incr     () = stat.unification_count <- stat.unification_count + 1
 let unification_time_incr t =
   stat.unification_time <- Mtime.Span.add stat.unification_time (t ())
@@ -211,6 +211,7 @@ module State =
     let prunes {prunes} = prunes
 
     let fresh {env; scope} = VarEnv.fresh ~scope env
+    let wc {env; scope} = VarEnv.wc ~scope env
 
     let new_scope st = {st with scope = Term.Var.new_scope ()}
 
@@ -279,14 +280,14 @@ let (===) x y st =
   | None    -> unification_time_incr t; failure st
 
 let unify = (===)
-          
+
 let (=/=) x y st =
   match State.diseq x y st with
   | Some st -> success st
   | None    -> failure st
 
 let diseq = (=/=)
-          
+
 let delay g st =
   delay_counter_incr ();
   RStream.from_fun (fun () -> g () st)
@@ -329,6 +330,10 @@ let conde = (?|)
 
 let call_fresh f st =
   let x = State.fresh st in
+  f x st
+
+let wc f st =
+  let x = State.wc st in
   f x st
 
 module Fresh =

PLTools/OCanren

src/core/Core.ml
@@ -278,6 +278,7 @@ module State =
     let prunes {prunes} = prunes
 
     let fresh {env; scope} = Env.fresh ~scope env
+    let wc {env; scope} = Env.wc ~scope env
 
     let new_scope st = {st with scope = Term.Var.new_scope ()}
 
@@ -481,6 +482,10 @@ let conde = (?|)
 
 let call_fresh f st =
   let x = State.fresh st in
+  f x st
+
+let wc f st =
+  let x = State.wc st in
   f x st
 
 module Fresh =