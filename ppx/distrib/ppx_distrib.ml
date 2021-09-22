13da536683acff8a2b40d0edd84095d6008d2c0c
kakadu <kakadu@pm.me>
2021-09-22 19:25:00 +0300

    Fix formatting of ppxes

--- ppx/distrib/ppx_distrib.ml
+++ ppx/distrib/ppx_distrib.ml
@@ -15,125 +15,41 @@ let () =
     let pattern =
       let open Ast_pattern in
       pstr
-        (pstr_type
-           nonrecursive
-           ((type_declaration_attributes __ @@  type_declaration
-              ~name:(string "t")
-              ~params:__
-              ~cstrs:nil
-              ~kind:__
-              ~private_:__
-              ~manifest:none)
-           ^:: nil)
-        ^:: pstr_type
-              __
-              (type_declaration
-                 ~name:(string "ground")
-                 ~params:nil
-                 ~cstrs:nil
-                 ~kind:__
-                 ~private_:__
-                 ~manifest:(some __)
-              ^:: nil)
-        ^:: nil)
-    in
-    [ Extension.declare
-        name
-        Extension.Context.Structure_item
-        pattern
-        (fun ~loc ~path attributes1 params1 kind1 private1 rec_2 kind2 private2 manifest2 ->
+        ( pstr_type nonrecursive
+            ( ( type_declaration_attributes __
+              @@ type_declaration ~name:(string "t") ~params:__ ~cstrs:nil
+                   ~kind:__ ~private_:__ ~manifest:none )
+            ^:: nil )
+        ^:: pstr_type __
+              ( type_declaration ~name:(string "ground") ~params:nil ~cstrs:nil
+                  ~kind:__ ~private_:__ ~manifest:(some __)
+              ^:: nil )
+        ^:: nil ) in
+    [ Extension.declare name Extension.Context.Structure_item pattern
+        (fun
+          ~loc
+          ~path
+          attributes1
+          params1
+          kind1
+          private1
+          rec_2
+          kind2
+          private2
+          manifest2
+        ->
           let open Ppxlib.Ast_builder.Default in
           let base_tdecl =
             let td =
-              type_declaration
-                ~loc
-                ~name:(Located.mk ~loc "t")
-                ~params:params1
-                ~cstrs:[]
-                ~private_:private1
-                ~kind:kind1
-                ~manifest:None
-            in
-            { td with ptype_attributes = attributes1 }
-          in
+              type_declaration ~loc ~name:(Located.mk ~loc "t") ~params:params1
+                ~cstrs:[] ~private_:private1 ~kind:kind1 ~manifest:None in
+            {td with ptype_attributes = attributes1} in
           let items =
-            Ppx_distrib_expander.process_main
-              ~loc
-              base_tdecl
+            Ppx_distrib_expander.process_main ~loc base_tdecl
               ( rec_2
-              , type_declaration
-                  ~loc
-                  ~params:[]
-                  ~cstrs:[]
-                  ~name:(Located.mk ~loc "ground")
-                  ~kind:kind2
-                  ~private_:private2
-                  ~manifest:(Some manifest2) )
-          in
-          pstr_include ~loc (include_infos ~loc (pmod_structure ~loc items)))
-    ]
-  in
-  Ppxlib.Driver.register_transformation
-    ~extensions
-    (*
-    ~impl:(fun ss ->
-      let open Ppx_distrib_expander in
-      let eval_si ~fail ~failtd hist si =
-        match si.pstr_desc with
-        | Pstr_type (flg, tydecls) ->
-          List.concat
-          @@ List.map tydecls ~f:(fun tydecl ->
-                 if suitable_tydecl tydecl
-                 then str_type_decl ~loc:si.pstr_loc (flg, tydecls)
-                 else if Option.is_some @@ is_super_suitable tydecl
-                 then
-                   (* let () = Attribute.explicitly_drop#type_declaration tydecl in *)
-                   process_super_suitable ~loc:si.pstr_loc hist (flg, tydecl)
-                 else
-                   [ { si with pstr_desc = Pstr_type (flg, List.map ~f:failtd tydecls) } ])
-        | _ -> fail si
-      in
-      let m =
-        object (self)
-          inherit Ast_traverse.map as super
-
-          method! structure ss =
-            List.fold_left
-              ss
-              ~f:(fun (hist, acc) si ->
-                ( si :: hist
-                , acc
-                  @ eval_si
-                      ~failtd:super#type_declaration
-                      ~fail:(fun si -> [ super#structure_item si ])
-                      hist
-                      si ))
-              ~init:([], [])
-            |> snd
-
-          (* List.concat @@ List.map ~f:(self#do_structure_item) ss *)
-
-          method do_structure_item si =
-            eval_si
-              ~failtd:super#type_declaration
-              ~fail:(fun si -> [ super#structure_item si ])
-              []
-              si
-          (*             match si.pstr_desc with
-            | Pstr_type (flg,tydecls) ->
-              List.concat @@ List.map tydecls
-                ~f:(fun tydecl ->
-                    if suitable_tydecl tydecl
-                    then str_type_decl ~loc:si.pstr_loc (flg, tydecls)
-                    else if is_super_suitable tydecl
-                    then process_super_suitable ~loc:si.pstr_loc (flg, tydecls)
-                    else [{si with
-                            pstr_desc = Pstr_type (flg, List.map ~f:(super#type_declaration) tydecls)}]
-                  )
-            | _ -> [super#structure_item si] *)
-        end
-      in
-      m#structure ss)
-      *)
-    name
-;;
+              , type_declaration ~loc ~params:[] ~cstrs:[]
+                  ~name:(Located.mk ~loc "ground") ~kind:kind2
+                  ~private_:private2 ~manifest:(Some manifest2) ) in
+          pstr_include ~loc (include_infos ~loc (pmod_structure ~loc items)) )
+    ] in
+  Ppxlib.Driver.register_transformation ~extensions name
--- ppx/wildcard/ppx_wildcard.ml
+++ ppx/wildcard/ppx_wildcard.ml
@@ -27,138 +27,6 @@ open Base
 open Ppxlib
 open Ppxlib.Ast_helper
 
-let is_state_pattern pat =
-  match pat.ppat_desc with
-  | Ppat_var v when String.equal v.txt "st" || String.equal v.txt "state" ->
-      Some v.txt
-  | _ -> None
-
-let classify_name ~f e =
-  match e.pexp_desc with Pexp_ident i when f i.txt -> true | _ -> false
-
-let need_insert_fname ~name e = classify_name e ~f:(Stdlib.( = ) (Lident name))
-(* match e.pexp_desc with
-   | Pexp_ident i when i.txt = Lident name -> true
-   | _ -> false *)
-
-let is_defer = need_insert_fname ~name:"defer"
-let is_conde = need_insert_fname ~name:"conde"
-let is_fresh = need_insert_fname ~name:"fresh"
-let is_call_fresh = need_insert_fname ~name:"call_fresh"
-
-let is_unif =
-  classify_name ~f:(function
-    | Lident s ->
-        String.length s >= 3 && String.equal (String.sub s ~pos:0 ~len:3) "==="
-    | _ -> false )
-
-let is_conj = need_insert_fname ~name:"conj"
-let is_conj_list = need_insert_fname ~name:"?&"
-
-let is_disj e =
-  need_insert_fname ~name:"disj" e || need_insert_fname ~name:"|||" e
-
-(*
-let rec walkthrough ~fname (expr: expression) =
-
-  let add_fname () =
-    [%expr [%e Ast_helper.Exp.constant (Pconst_string (fname,None))] <=>
-           [%e expr]
-    ]
-  in
-  match expr.pexp_desc with
-  | Pexp_fun (_label, _opt, pat, e2) -> begin
-      match is_state_pattern pat with
-      | None ->
-        { expr with pexp_desc =
-                   Pexp_fun (_label, _opt, pat, walkthrough ~fname e2) }
-      | Some argname ->
-        (* printf "found good function with statearg '%s'\n%!" argname; *)
-        let new_body =
-          [%expr
-             let () = Printf.printf "entering '%s'\n%!" [%e Ast_helper.Exp.constant (Pconst_string (fname,None))] in
-             let ans = [%e e2] in
-             let () = Printf.printf "leaving '%s'\n%!"  [%e Ast_helper.Exp.constant (Pconst_string (fname,None))] in
-             ans
-          ]
-        in
-        { expr with pexp_desc= Pexp_fun (_label, _opt, pat, new_body) }
-    end
-  | Pexp_apply (e,_) when is_call_fresh e -> add_fname ()
-  | Pexp_apply (e,_) when is_disj e -> add_fname ()
-  | Pexp_apply (e,_) when is_conj e -> add_fname ()
-
-  | _ -> expr
-
-
-let map_value_binding (vb : value_binding) =
-  match vb.pvb_pat.ppat_desc with
-  | Ppat_var name ->
-    let fname = name.txt in
-    { vb with pvb_expr = walkthrough ~fname vb.pvb_expr }
-  |  _ -> vb
-
-let smart_logger =
-  { default_mapper with
-    structure_item = fun mapper sitem ->
-      match sitem.pstr_desc with
-      | Pstr_value (_rec, vbs) ->
-        { sitem with pstr_desc = Pstr_value (_rec, List.map vbs ~f:map_value_binding) }
-      | x -> default_mapper.structure_item mapper sitem
-  }
-*)
-
-let option_map ~f = function Some x -> Some (f x) | None -> None
-let option_bind ~f = function Some x -> f x | None -> None
-
-exception Not_an_ident
-
-let reconstruct_args e =
-  let open Longident in
-  let are_all_idents (xs : (_ * expression) list) =
-    try
-      Some
-        (List.map xs ~f:(fun (_, e) ->
-             match e.pexp_desc with
-             | Pexp_ident {txt = Longident.Lident i; _} -> i
-             | _ -> raise Not_an_ident ) )
-    with Not_an_ident -> None in
-  match e.pexp_desc with
-  | Pexp_apply ({pexp_desc = Pexp_ident {txt = Longident.Lident arg1; _}}, ys)
-    ->
-      (* fresh (var1 var2 var3) body *)
-      option_map (are_all_idents ys) ~f:(fun xs -> arg1 :: xs)
-  (* no fresh variables: just for geting rid of &&&  *)
-  | Pexp_construct ({txt = Lident "()"}, None) -> Some []
-  (* [fresh arg0 body] -- single fresh variable  *)
-  | Pexp_ident {txt = Lident arg1; _} -> Some [arg1]
-  | _ -> None
-
-let list_fold ~f ~initer xs =
-  match xs with
-  | [] -> failwith "bad argument"
-  | start :: xs -> List.fold ~init:(initer start) ~f xs
-
-let list_fold_right0 ~f ~initer xs =
-  let rec helper = function
-    | [] -> failwith "bad_argument"
-    | x :: xs -> list_fold ~initer ~f:(fun acc x -> f x acc) (x :: xs) in
-  helper (List.rev xs)
-
-let my_list ~loc es =
-  List.fold_right ~init:[%expr []] es ~f:(fun x acc ->
-      [%expr [%e x] :: [%e acc]] )
-
-let parse_to_list alist =
-  let rec helper acc ele =
-    match ele.pexp_desc with
-    | Pexp_construct ({txt = Lident "[]"}, None) -> acc
-    | Pexp_construct
-        ({txt = Lident "::"}, Some {pexp_desc = Pexp_tuple [y1; y2]; _}) ->
-        helper (y1 :: acc) y2
-    | x -> [ele] in
-  List.rev @@ helper [] alist
-
 let name_of_loc loc =
   (* Format.printf "name_of_loc: %a\n%!" Location.print loc; *)
   let start = loc.Location.loc_start in
@@ -181,11 +49,10 @@ let wildcard_extractor expr =
           (pexp_ident (lident (string "__")))
           loc e
           (on_OK, e.pexp_loc :: acc)
-          ~on_error:(fun () ->
-            (* Format.printf "%s %d: matching failed on `%a`\n%!" __FILE__ __LINE__ Pprintast.expression e; *)
-            super#expression e acc)
+          ~on_error:(fun () -> super#expression e acc)
     end in
   folder#expression expr []
+
 let mapper =
   object (self)
     inherit Ast_traverse.map as super
@@ -199,24 +66,17 @@ let mapper =
           (pexp_ident (lident (string "===")))
           ((nolabel ** __) ^:: (nolabel ** __) ^:: nil) in
       let on_unif l r =
-        (* Format.printf "%s %d: got unification (`%a` === `%a`)\n%!" __FILE__ __LINE__ Pprintast.expression l Pprintast.expression r; *)
         let l, accl = wildcard_extractor l in
-        (* Format.printf "accl = %a\n%!"  (Format.pp_print_list Location.print) accl; *)
         let r, accr = wildcard_extractor r in
-        (* Format.printf "accr = %a\n%!" (Format.pp_print_list Location.print) accr; *)
         let f acc loc =
           let open Ppxlib.Ast_builder.Default in
           [%expr
             wc (fun [%p ppat_var ~loc (Located.mk ~loc (name_of_loc loc))] ->
                 [%e acc] )] in
-        let ans1 =
-          List.fold_left ~f
-            ~init:[%expr [%e l] === [%e r]]
-            accr
-        in
-        List.fold_left ~f ~init:ans1 accl
-      in
-      Ppxlib.Ast_pattern.parse pat loc e on_unif ~on_error:(fun () -> super#expression e)
+        let ans1 = List.fold_left ~f ~init:[%expr [%e l] === [%e r]] accr in
+        List.fold_left ~f ~init:ans1 accl in
+      Ppxlib.Ast_pattern.parse pat loc e on_unif ~on_error:(fun () ->
+          super#expression e )
   end
 
 let () =
