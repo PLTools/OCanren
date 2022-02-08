6806a9218f8a989c5e175d85e3359c200175f424
Kakadu <Kakadu@pm.me>
2022-02-08 13:37:51 +0300

    Tester may require some more polishing too
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- tester/tester.ml
+++ tester/tester.ml
@@ -5,25 +5,20 @@ open OCanren
 
 (** {3 Helper functions to provide names for top-level variables } *)
 
-(* let qh    = fun qs          -> ["q", qs] *)
-(* let qrh   = fun qs rs       -> ["q", qs; "r", rs] *)
+let wrap onOK i (name, x) =
+  onOK i name x
 
-let wrap onOK onFree i (name, x) =
-  if x#is_open
-  then onFree i name x
-  else onOK i name x
+let qh onOK = fun q () ->
+  List.iteri (wrap onOK) @@ ["q", q]
 
-let qh onOK onFree = fun q () ->
-  List.iteri (wrap onOK onFree) @@ ["q", q]
+let qrh onOK = fun q r () ->
+  List.iteri (wrap onOK) @@ ["q", q; "r", r]
 
-let qrh onOK onFree = fun q r () ->
-  List.iteri (wrap onOK onFree) @@ ["q", q; "r", r]
+let qrsh onOK = fun q r s () ->
+  List.iteri (wrap onOK) @@ ["q", q; "r", r; "s", s]
 
-let qrsh onOK onFree = fun q r s () ->
-  List.iteri (wrap onOK onFree) @@ ["q", q; "r", r; "s", s]
-
-let qrsth onOK onFree = fun q r s t () ->
-  List.iteri (wrap onOK onFree) @@ ["q", q; "r", r; "s", s; "t", t]
+let qrsth onOK = fun q r s t () ->
+  List.iteri (wrap onOK) @@ ["q", q; "r", r; "s", s; "t", t]
 
 let make_title n msg =
   printf "%s, %s answer%s {\n%!"
@@ -33,7 +28,7 @@ let make_title n msg =
 
 exception NoMoreAnswers
 
-let run_gen onOK onFree n num handler (repr, goal) =
+let run_gen onFree n num handler (repr, goal) =
   make_title n repr;
   let rec loop st = function
   | k when (k > n) && (n >= 0) -> ()
@@ -46,34 +41,12 @@ let run_gen onOK onFree n num handler (repr, goal) =
       loop tl (k+1)
     | _ -> assert false
   in
-  let handler = handler onOK onFree in
+  let handler = handler onFree in
   let () = try loop (run num goal handler) 1 with NoMoreAnswers -> () in
   printf "}\n%!"
 
-(**
-  [run_exn printer n name_helper goal] prints answers supposing there are no free variables there
-  (i.e. reification is not required)
-*)
-let run_exn printer = run_gen
-  (fun i name x -> printf "%s%s=%s;%!" (if i<>0 then " " else "") name (printer x#prj) )
-  (fun _ _ _ -> failwith "Free logic variables in the answer")
-
-(**
-  [runR reifier print_plain print_injected n name_helper goal] prints answers both with free varibles and
-  without them. In the first cases it uses [print_plain] as printer fuction. In the latter case it does
-  reification using [reifier] and prints the result wit [print_ibjected]
-*)
-let runR reifier printerNoFree printerR = run_gen
-  (fun i name x -> printf "%s%s=%s;%!" (if i<>0 then " " else "") name (printerNoFree x#prj) )
-  (fun i name func ->
+let run_r reifier printerR = run_gen
+  (fun i name (func : _ OCanren.reified) ->
     let ans = func#reify reifier in
     printf "%s%s=%s;%!" (if i<>0 then " " else "") name (printerR ans)
     )
-
-let run_prjc reifier printer = run_gen
-  (fun i name x ->
-     printf "%s%s=%s;%!" (if i<>0 then " " else "") name (printer x#prj) )
-  (fun i name func ->
-    let ans = func#prjc reifier in
-    printf "%s%s=%s;%!" (if i<>0 then " " else "") name (printer ans)
-  )
