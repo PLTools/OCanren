83ecbf0092173fefe0d8e9ee45070129f61d9519
Kakadu <Kakadu@pm.me>
2022-02-08 13:08:53 +0300

    PPX rewriters: new Distrib
    Signed-off-by: Kakadu <Kakadu@pm.me>

---
+++ ppx/pp_distrib_gt.ml
@@ -0,0 +1,12 @@
+(*
+ * OCanren PPX
+ * Copyright (C) 2016-2021
+ *   Dmitrii Kosarev aka Kakadu
+ * St.Petersburg State University, JetBrains Research
+ *)
+
+let () =
+  (* TODO: Invent something for margins
+  https://github.com/ocaml-ppx/ppxlib/issues/273*)
+  Ppxlib.Driver.standalone ()
+;;
--- ppx/repr/ppx_repr.ml
+++ ppx/repr/ppx_repr.ml
@@ -1,6 +1,6 @@
 (*
  * OCanren. PPX suntax extensions.
- * Copyright (C) 2015-2020
+ * Copyright (C) 2015-2021
  * Dmitri Boulytchev, Dmitry Kosarev, Alexey Syomin, Evgeny Moiseenko
  * St.Petersburg State University, JetBrains Research
  *
@@ -23,21 +23,23 @@ let string_of_expression e =
   Format.set_max_indent 2000;
   let ans = Format.asprintf "%a" Pprintast.expression e in
   ans
+;;
 
-let mapper = object
-  inherit Ast_traverse.map as super
+let mapper =
+  object
+    inherit Ast_traverse.map as super
 
-  method! expression e =
-    match e with
-    | { pexp_desc=Pexp_construct ({txt=Lident "REPR";_}, Some e); _} as expr ->
+    method! expression e =
+      match e with
+      | { pexp_desc = Pexp_construct ({ txt = Lident "REPR"; _ }, Some e); _ } as expr ->
+        let text = string_of_expression e in
+        { expr with
+          pexp_desc =
+            Pexp_tuple
+              [ Ast_helper.Exp.constant (Pconst_string (text, e.pexp_loc, None)); e ]
+        }
+      | e -> super#expression e
+  end
+;;
 
-      let text = string_of_expression e in
-      { expr with pexp_desc =
-                    Pexp_tuple [Ast_helper.Exp.constant (Pconst_string (text, e.pexp_loc, None)); e] }
-    | e -> super#expression e
-end
-
-let () =
-  Ppxlib.Driver.register_transformation
-    ~impl:(fun s  -> mapper#structure s)
-    "repr"
+let () = Ppxlib.Driver.register_transformation ~impl:(fun s -> mapper#structure s) "repr"
