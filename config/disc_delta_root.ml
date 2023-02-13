eb62e07b7428a44da0925a7e5afc7470896f4307
Kakadu <Kakadu@pm.me>
2023-02-14 01:24:48 +0300

    Fix path issue about vendoring
    If OCanren is vendored tests are exceuted from another dir,
    and standalone rewriter is not beiung discovered.
    
    See also: https://github.com/ocaml/dune/issues/7043
    
    Signed-off-by: Kakadu <Kakadu@pm.me>

---
+++ config/disc_delta_root.ml
@@ -0,0 +1,26 @@
+let prj = Sys.argv.(1)
+let ws = Sys.argv.(2)
+
+let chop_prefix s ~prefix =
+  if String.starts_with ~prefix s then
+    StringLabels.sub s ~pos:(String.length prefix)
+      ~len:(String.length s - String.length prefix)
+  else assert false
+
+let () =
+  (* print_endline prj;
+     print_endline ws;
+     print_endline (Unix.realpath prj);
+     print_endline (Unix.realpath ws); *)
+  let ans =
+    if prj = ws then "."
+    else
+      let ans = chop_prefix (Unix.realpath prj) ~prefix:(Unix.realpath ws) in
+      if String.starts_with ans ~prefix:"/" then chop_prefix ans ~prefix:"/"
+      else ans
+  in
+
+  (* Format.printf "ans = %s\n%!" ans; *)
+  Stdlib.Out_channel.with_open_text "delta-path.cfg" (fun ch ->
+    output_string ch ans);
+  ()
